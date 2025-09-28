ec7ad052a409e5e5271917d0b0c4102d9789a5d2
abramov222 <ilyabramov22222@gmail.com>
2025-09-28 18:46:07 +0700

    Add class Balance

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
 public class Main {
 	public static void main(String[] args)
 	{
+		System.out.println("1 Задание:\n");
 		int counter = 0;
 		Button button = new Button();
 		
 		button.click();
 		button.click();
 		button.click();
+		
+		System.out.println("-----------------------------------------------------------\n2 Задание:\n");
+		
+		 Balance balance = new Balance();
+	        
+	        balance.addRight(10);
+	        balance.addLeft(5);
+	        balance.result();
+	        
+	        balance.addLeft(5);
+	        balance.result(); 
+	        
+	        balance.addLeft(5);
+	        balance.result(); 
+	        
+	        balance.addLeft(99);
+	        balance.result(); 
 	}
+	
 }
